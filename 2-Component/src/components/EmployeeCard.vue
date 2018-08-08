e632261c20c0442cce2e983ec414d1403537acf8
spontoreau <pontoreau.sylvain@gmail.com>
2018-08-08 16:10:43 +0200

    Add props in EmployeeCard component

--- 2-Component/src/components/EmployeeCard.vue
+++ 2-Component/src/components/EmployeeCard.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="card pb-2">
-    <img class="card-img-top" src="http://localhost:3000/images/m1.png" alt="Card image cap">
+    <img class="card-img-top" :src="pictureUrl()" alt="Card image cap">
     <div class="card-body">
       <div>
         <label class="card-text">
           <i class="fas fa-user-circle"></i>
-          Thomas A. Anderson
+          {{ fullName }}
         </label>
       </div>
       <div>
         <label class="card-text">
           <i class="fas fa-phone"></i>
-          646-226-1860
+          {{ phone }}
         </label>
       </div>
       <div>
         <label class="card-text">
           <i class="far fa-calendar"></i>
-          Aug 8th 18
+          {{ formattedDate() }}
         </label>
       </div>
       <div class="pt-4">
         <span class="badge float-left" style="backgroundColor: #0984e3; color: #FFFFFF;">Executive</span>
-        <a class="float-right" href="mailto:homas.anderson@misterandersoncorporation.com">
+        <a class="float-right" :href="mailTo()">
           <i class="far fa-envelope"></i>
         </a>
       </div>
@@ -33,10 +33,38 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+// @ts-ignore
+import moment from 'moment';
 
 @Component
 export default class EmployeeCard extends Vue {
+  @Prop()
+  fullName!: string;
 
+  @Prop()
+  hiredDate!: string;
+
+  @Prop()
+  phone!: string;
+
+  @Prop()
+  picture!: string;
+
+  @Prop()
+  email!: string;
+
+  formattedDate() {
+    return moment(this.hiredDate).format('MMM Do YY');
+  }
+
+  pictureUrl() {
+    return `${ process.env.VUE_APP_PICTURE_URL }${this.picture}`;
+  }
+
+  mailTo() {
+    return `mailto:${this.email}`;
+  }
 }
 </script>
 
--- 2-Component/src/views/Home.vue
+++ 2-Component/src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <div class="row pt-4">
-      <EmployeeCard />
+      <div class="card-columns">
+        <EmployeeCard 
+          :fullName="'Thomas A. Anderson'"
+          :phone="'646-226-1860'"
+          :hiredDate="'1999-03-31T00:00:00Z'"
+          :email="'thomas.anderson@misterandersoncorporation.com'"
+          :picture="'m1.png'"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -13,7 +21,7 @@ import EmployeeCard from '@/components/EmployeeCard.vue';
 @Component({
   components: {
     EmployeeCard,
-  }
+  },
 })
 export default class Home extends Vue {}
 </script>
