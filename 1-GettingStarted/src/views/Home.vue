551fb9f7c2b76f45c6c7583b80fec788d733736a
spontoreau <pontoreau.sylvain@gmail.com>
2018-08-08 09:50:15 +0200

    Base template

--- 1-GettingStarted/src/views/Home.vue
+++ 1-GettingStarted/src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="row pt-4">
+    <h5>Employee directory</h5>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component
 export default class Home extends Vue {}
 </script>
