d67491f420fad373c9ae543b88e9fc98ad531cc5
spontoreau <pontoreau.sylvain@gmail.com>
2018-08-10 16:47:06 +0200

    Add search bar

--- 3-Vuex/src/components/Navigation.vue
+++ 3-Vuex/src/components/Navigation.vue
@@ -4,6 +4,16 @@
       <img src="../assets/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
       Mister Anderson Corporation
     </a>
+    <form class="form-inline">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="search">
+            <i class="fas fa-search"></i>
+          </span>
+        </div>
+        <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="search">
+      </div>
+    </form>
   </nav>
 </template>
 
