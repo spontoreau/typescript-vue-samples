1a2dc3006b13d4c2a123a43d9492e8ed62537347
spontoreau <pontoreau.sylvain@gmail.com>
2018-08-10 10:23:08 +0200

    Create store folder

---
+++ 2-Vuex/src/store/index.ts
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Vuex, { StoreOptions } from 'vuex';
+
+Vue.use(Vuex);
+
+const storeOptions: StoreOptions<{}> = {
+  strict: process.env.NODE_ENV !== 'production',
+  state: {},
+  modules: {
+
+  }
+}
+
+export default new Vuex.Store(storeOptions);
