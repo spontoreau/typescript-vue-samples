bda72598a15d485bdcfbd9fd9c9d0fea13b48621
spontoreau <pontoreau.sylvain@gmail.com>
2018-08-12 22:27:23 +0200

    Convert TeamBadge component to a functional component

--- 3-Vuex/src/components/TeamBadge.vue
+++ 3-Vuex/src/components/TeamBadge.vue
@@ -1,17 +1,14 @@
-<template>
-  <span class="badge" :style="{ color: team.textColor, backgroundColor: team.tagColor }">
-    {{ team.name }}
+<template functional>
+  <span class="badge" :style="{ color: props.team.textColor, backgroundColor: props.team.tagColor }">
+    {{ props.team.name }}
   </span>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import Component from 'vue-class-component';
-import { Prop } from 'vue-property-decorator';
 import { Team } from '@/store/modules/employee/retrieve/types/team';
 
-@Component
-export default class TeamBadge extends Vue {
-  @Prop() team!: Team;
-}
+export default Vue.extend<Team>({
+  functional: true,
+});
 </script>
