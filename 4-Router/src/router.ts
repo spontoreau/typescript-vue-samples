a577eb3a1834cb87084128bc24e1cb0f99705cc9
spontoreau <pontoreau.sylvain@gmail.com>
2018-08-16 16:38:17 +0200

    Add NotFound view and resolve it with Lazy loading

--- 4-Router/src/router.ts
+++ 4-Router/src/router.ts
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 
+const NotFound = () => import('./views/NotFound.vue');
+
 Vue.use(Router);
 
 export default new Router({
@@ -12,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '*',
+      name: '404',
+      component: NotFound,
     },
   ],
 });
---
+++ 4-Router/src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template functional>
+  <div class="pt-4">
+    <h5>Page not found</h5>
+    <div>
+      <p>
+          This is not the page you are looking for...
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  functional: true,
+});
+</script>
